567071d0e5b135544ae80c5b0287cad4ae96d4fd
Phillip <phv9495@saintjoe.edu>
2015-02-04 14:11:10 -0500

    Add in all of the files needed.

--- company.sql
+++ company.sql
@@ -30,27 +30,88 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
--- Name: phones; Type: TABLE; Schema: public; Owner: eaglesreign; Tablespace: 
+-- Name: department; Type: TABLE; Schema: public; Owner: brianc; Tablespace: 
 --
 
-CREATE TABLE phones (
-    name character varying,
-    brand character varying,
-    cost integer,
-    year_purchased integer
+CREATE TABLE employee (
+    fname character varying(15) NOT NULL,
+    minit character(1),
+    lname character varying(15) NOT NULL,
+    ssn character(9) NOT NULL,
+    bdate date,
+    address character varying(30),
+    sex character(1),
+    salary numeric(10,2),
+    super_ssn character(9),
+    dno integer NOT NULL
 );
 
 
-ALTER TABLE phones OWNER TO eaglesreign;
+ALTER TABLE ONLY employee
+    ADD CONSTRAINT employee_pkey PRIMARY KEY (ssn);
+
+
+
+CREATE TABLE department (
+    dname character varying(15) NOT NULL,
+    dnumber integer NOT NULL,
+    mgr_ssn character(9) NOT NULL,
+    mgr_start_date date
+);
+
+CREATE TABLE dept_locations (
+    dnumber integer NOT NULL,
+    dlocation character varying(15) NOT NULL
+);
+
+CREATE TABLE dependent (
+    essn character(9) NOT NULL,
+    dependent_name character varying(15) NOT NULL,
+    sex character(1),
+    bdate date,
+    relationship character varying(8)
+);
+
+CREATE TABLE WORKS_ON (
+	Essn character(9) NOT NULL,
+	Pno integer NOT NULL,
+	Hours decimal(3,1) NOT NULL
+);
+
+CREATE TABLE PROJECT (
+	Pname varchar(15) NOT NULL,
+	Pnumber integer NOT NULL,
+	Plocation varchar(15) NOT NULL,
+	Dnum integer NOT NULL
+);
+
+
 
 --
--- Data for Name: phones; Type: TABLE DATA; Schema: public; Owner: eaglesreign
+-- Data for Name: department; Type: TABLE DATA; Schema: public; Owner: brianc
 --
 
-COPY phones (name, brand, cost, year_purchased) FROM stdin;
-Galaxy S3	Samsung	80	2013
+COPY department (dname, dnumber, mgr_ssn, mgr_start_date) FROM stdin;
 \.
 
+
+--
+-- Name: department_dname_key; Type: CONSTRAINT; Schema: public; Owner: brianc; Tablespace: 
+--
+
+ALTER TABLE ONLY department
+    ADD CONSTRAINT department_dname_key UNIQUE (dname);
+
+ALTER TABLE ONLY dept_locations
+    ADD CONSTRAINT dept_locations_pkey PRIMARY KEY (dnumber, dlocation);
+
+
+--
+-- Name: department_pkey; Type: CONSTRAINT; Schema: public; Owner: brianc; Tablespace: 
+--
+
+ALTER TABLE ONLY department
+    ADD CONSTRAINT department_pkey PRIMARY KEY (dnumber);
 
 --
 -- Name: public; Type: ACL; Schema: -; Owner: postgres
@@ -65,4 +126,4 @@ GRANT ALL ON SCHEMA public TO PUBLIC;
 --
 -- PostgreSQL database dump complete
 --
-
+--
